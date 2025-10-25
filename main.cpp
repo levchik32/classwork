553c85ddf0acfa1fce288de6d8309ea5061c128d
Carrot_lord <lev.saldaev@gmai.com>
2025-10-25 14:26:55 +0300

    output complete

--- main.cpp
+++ main.cpp
@@ -4,4 +4,14 @@ int main()
 {
     unsigned a = 0;
     std::cin >> a;
+
+    if (std::cin.eof())
+    {
+        std::cout << 0 << "\n";
+    }
+    else if (std::cin.fail())
+    {
+        std::cerr << "Input err\n";
+        return 1;
+    }
 }
