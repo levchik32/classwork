62ca91a451648f71816be60826571a29c26273d0
Carrot_lord <lev.saldaev@gmai.com>
2025-10-25 14:55:50 +0300

    cunction isPyth complete

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
+bool isPyth(unsigned a, unsigned b, unsigned c)
+{
+    bool condit = a*a == b*b + c*c;
+    condit = condit || b*b == a*a + c*c;
+    condit = condit || c*c == b*b + a*a;
+    return condit;
+}
+
 int main()
 {
-    unsigned a = 0;
+    unsigned a = 0, b = 0, c = 0;
     size_t count = 0;
     while (std::cin >> a)
     {
-        count++;
+        count += isPyth(a, b, c)?1:0;
     }
 
     if (std::cin.eof())
